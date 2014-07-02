21852bf552b8989c7208db17dfd0eeba9bd05fd8
Mark Reiter <markr1197@gmail.com>
2014-07-01 19:01:19 -0500

    First commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def team
+  end
+
+  def web
+  end
+
+  def app
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Downturn::Application.config.secret_key_base = '80376895b8cafc029384aa1858df0c583e99b29541450fe86bca5186e48f133f68fc5264bfe52144aa742d2cfded16710fbcfa94d25cbb9c6aa892039edeb6c6'
